e843fc6a1fb3083ce8f1df29752da86693b87f8e
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-05-01 14:40:00 +0530

    Unwanted file deleted

--- 1.Registration Form/make.js
+++
@@ -1,9 +0,0 @@
-
-const init = function(){
-
-    let username = document.getElementById("Username").value;
-    let Email = document.getElementById("Email").value;
-    let Phone = document.getElementById("Phone").value;
-
-    alert(username,Email,Phone)
-}
