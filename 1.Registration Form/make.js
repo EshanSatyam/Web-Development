7b7987738ff66b3c3fd7dd9058ac7510024608be
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-05-01 14:39:25 +0530

    Code Enhanced

---
+++ 1.Registration Form/make.js
@@ -0,0 +1,9 @@
+
+const init = function(){
+
+    let username = document.getElementById("Username").value;
+    let Email = document.getElementById("Email").value;
+    let Phone = document.getElementById("Phone").value;
+
+    alert(username,Email,Phone)
+}
